aa55884d693c92da6dc96eb7a45c9ecd774fefc2
David Wolever <david@wolever.net>
2009-03-03 13:04:55 -0500

    Adding tests, looks like the first version might work

--- randchk.py
+++ randchk.py
@@ -1,27 +1,117 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.5
+from __future__ import with_statement
 
-import os
-from os import path
+from os import listdir, path, stat
 from optparse import OptionParser
+from random import randint
+from hashlib import md5
+
+class randlist(list):
+    def __iter__(self):
+        """ Return random items while being iterated over.
+            Note that it is ok to add items between yields. """
+        while len(self):
+            topop = randint(0, len(self) - 1)
+            yield self.pop(topop)
 
 def assert_dir(f):
     """ Assert that 'f' is a directory. """
     assert path.isdir(f), "%r is not a directory!" %(f)
 
+def joinmany(dir, files):
+    """ Join one directory to many files.
+        >>> joinmany("/tmp/", ['a', 'b'])
+        ['/tmp/a', '/tmp/b']
+        >>> """
+    return [ path.join(dir, file) for file in files ]
+
 def randomly_walk_files(dir):
     """ Recursively yield all the files in 'dir', randomly ordering them. """
-    files = randlist(os.listdir(dir))
+    # XXX: Doesn't check for things like cyclic symlinks
+    files = randlist(joinmany(dir, listdir(dir)))
+    for file in files:
+        if path.isdir(file):
+            files.extend(joinmany(file, listdir(file)))
+            continue
+        yield file
 
-def compare_directories(left, right):
-    assert_dir(left)
-    assert_dir(right)
+def switchprefix(old, new, path):
+    """ Switch the prefix of "path" from "old" to "new". """
+    assert path.startswith(old)
+    return new + path[len(old):]
 
+def file_size(file):
+    """ Get a file's size. """
+    return stat(file).st_size
 
-# Parse the command line arguments
-parser = OptionParser(usage = "usage: %prog [options] LEFT RIGHT\n"
-    "\tRandomly checksum files in directories LEFT and RIGHT to ensure\n"
-    "\tthat they are the same.")
-    
-parser.add_option("-1024", "--first-1024", action="store_true", dest="first1024",
-                  help="Only check the first 1024 bytes of each file.")
-(options, args) = parser.parse_args()
+def file_checksum(file):
+    """ Checksum a file. """
+    with open(file) as f:
+        sum = md5()
+        d = f.read(1024)
+        while d:
+            sum.update(d)
+            d = f.read(1024)
+        return sum.hexdigest()
+
+def compare_files(*files):
+    """ Compare a list of files, assuming that the first is considered
+        "canonical" (that is, each file will be compared against the first).  A
+        list of (file name, error description) tuples is reurned. """
+
+    # A list of checks that will be performed.
+    # If any check fails, no further checks are processed.
+    # (this does have shortcomings, but it will do for now)
+    for check in (file_size, file_checksum):
+        problems = []
+
+        # Check the "canonical" file.
+        # Let any exceptions which happen here bubble up.
+        canonical = check(files[0])
+
+        # Check each individual file
+        for file in files[1:]:
+            problem = None
+            try:
+                result = check(file) 
+                if canonical != result:
+                    problem = "%s %s (expected) != %s (actual)" \
+                               %(check.__name__, canonical, result)
+            except EnvironmentError, e:
+                problem = e.strerror
+
+            if problem:
+                problems.append((file, problem))
+                problem = None
+
+        if problems:
+            return problems
+
+    return []
+
+def check_directories(dirs):
+    """ Randomly walk over the "canonical" files in dirs[0], comparing them to
+        the files in dirs[1:].  Triples of (canonical file path, problem file path,
+        problem description) are yielded. """
+    map(assert_dir, dirs)
+    dirs = map(path.normpath, dirs)
+    source_dir = dirs[0]
+    dest_dirs = dirs[1:]
+    for source in randomly_walk_files(source_dir):
+        dests = [switchprefix(source_dir, dest_dir, source) for dest_dir in dest_dirs]
+        problems = compare_files(source, *dests)
+        for (problem_file, problem_description) in problems:
+            yield source, problem_file, problem_description
+
+if __name__ == '__main__':
+    # Parse the command line arguments
+    parser = OptionParser(usage = "usage: %prog [options] CANONICAL CHECK...\n"
+        "\tRandomly checksum files in CANONICAL, comparing them to the same\n"
+        "\tfile in each CHECK ensuring that they are the same.")
+        
+    #parser.add_option("-1", "--first-1024", action="store_true", dest="first1024",
+    #                  help="Only check the first 1024 bytes of each file.")
+    (options, args) = parser.parse_args()
+    assert len(args) == 2
+    for (canonical_file, problem_file, description) in check_directories(args):
+        print "%s: %s (%s)" %(problem_file, description, canonical_file)
--- tests/test_randchk.py
+++ tests/test_randchk.py
@@ -1,17 +1,22 @@
-#!/usr/bin/env nosetests
+# If this file is run through nose, .. will automatically be added to sys.path.
+from randchk import *
 
-from ..randchk import randlist, assert_dir, randomly_walk_files, switchprefix
-
-from nose.tools import assert_equal, assert_raises
-import os
+from re import match
+from glob import glob
+from nose.tools import assert_equal, assert_raises, nottest
 from os import path
+from os import walk as oswalk
 from os.path import dirname
 
 def sanewalk(dir):
     """ A sane version of os.walk, yielding just paths. """
-    for (root, dirs, files) in os.walk(dir):
+    for (root, dirs, files) in oswalk(dir):
         for file in files:
             yield path.join(root, file)
+
+def helper_dir(name):
+    """ Return the name of a subdirectory of this test directory. """
+    return path.join(dirname(__file__), name)
 
 def test_randlist():
     # Check that itering over the list works, even when items are being added
@@ -29,7 +34,7 @@ def test_assert_dir():
     assert_raises(AssertionError, assert_dir, __file__)
 
 def test_randomly_walk_files():
-    testdir = path.join(dirname(__file__), "test_randomly_walk_files")
+    testdir = helper_dir("randomly_walk_files_helper")
     expected = set(sanewalk(testdir))
     actual = set(randomly_walk_files(testdir))
     assert_equal(expected, actual)
@@ -38,3 +43,50 @@ def test_switchprefix():
     assert_equal(switchprefix("foo", "blamo", "foo/bar"), "blamo/bar")
     assert_equal(switchprefix("blamo", "foo", "blamo/bar"), "foo/bar")
     assert_raises(AssertionError, switchprefix, "foo", "bar", "bar/baz")
+
+def directory_tests():
+    # Note that glob is smart about expanding slashes so this will work as
+    # expected on Windows
+    for dir in glob(helper_dir("test_*/")):
+        yield (run_directory_test, dir)
+
+@nottest
+def run_directory_test(directory):
+    # Set the description of this function so Nose will give it a sensible name
+    readme = open(path.join(directory, "README")).read()
+
+    # Problem descriptions have two parts: a file name and a pattern which
+    # should be part of the description.  They look like this:
+    #     Problem: path/to/problem/file, pattern in the description
+    def parse_problem(line):
+        """ Parse a "problem" line, returning (file, pattern). """
+        assert line.startswith("Problem:")
+        parts = [part.strip() for part in line[len("Problem:"):].split(",")]
+        assert len(parts) == len(filter(None, parts)), \
+               "Invalid 'Problem:' line: " + line
+        return parts
+
+    expected_problems = [parse_problem(line) for line in readme.splitlines()
+                         if line.startswith("Problem:")]
+   
+    # These are the directories which will be passed to check_directories.
+    # They are ordered alphanumerically, so let's hope the "canonical"
+    # directory is first.
+    test_directories = sorted(glob(path.join(directory, "*/")))
+    problems = list(check_directories(test_directories))
+
+    for (_, problem_file, problem_description) in problems:
+        found = False
+        for (eid, (e_file, e_description)) in enumerate(expected_problems):
+            # We only check that the problem file *ends* with the expeced file
+            # (makes life a bit easier, I think) 
+            if not problem_file.endswith(e_file): continue
+            found = True
+            assert e_description in problem_description, \
+                   "Expected description, %r, not in actual description, %r." \
+                    %(e_description, problem_description)
+            expected_problems.pop(eid)
+            break
+
+        assert found, "Unexpected problem encountered: %s: %s" \
+                      %(problem_file, problem_description)
