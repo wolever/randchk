c95890cbca1c65016196b74d167d1a7a21d2c963
David Wolever <david@wolever.net>
2009-07-26 10:43:12 -0400

    Adding __eq__ and __hash__ methods to File.

--- librandchk/master.py
+++ librandchk/master.py
@@ -10,15 +10,34 @@ class File(object):
         Or, a File represents a file which should be common to all slaves. """
 
     # We could have a lot of Files, so define which slots we'll be using.
-    __slots__ = [ 'type', 'path' ]
+    __slots__ = [ '_type', '_path', '_hashcode' ]
 
     types = [ "REG", "DIR", "LNK", "BLK", "CHR", "FIFO", "SOCK" ]
 
     def __init__(self, type, path):
         if type not in File.types:
             raise Exception("Invalid type: %r" %(type))
-        self.type = type
-        self.path = path
+        self._hashcode = None
+        self._type = type
+        self._path = path
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def path(self):
+        return self._path
+
+    def __hash__(self):
+        if self._hashcode is None:
+            self._hashcode = ( self.type, self.path ).__hash__()
+        return self._hashcode
+
+    def __eq__(self, other):
+        if not isinstance(other, File):
+            return false
+        return other.type == self.type and other.path == self.path
 
     def __repr__(self):
         return "<File path=%r type=%r>" %(self.path, self.type)
---
+++ tests/test_master.py
@@ -0,0 +1,35 @@
+from librandchk.master import File
+
+from nose.tools import raises, assert_equal, assert_not_equal
+
+@raises(AttributeError)
+def test_file_immutability():
+    f = File("DIR", "/")
+    # Files are immutable, so this should raise an exception
+    f.path = "/foo"
+
+@raises(Exception)
+def test_file_bad_type_check():
+    # An exception should be raised if we're instanciating
+    # File with a bad type
+    File("BAD", "/")
+
+def test_file_good_types():
+    # All the 'good' types should work, though
+    for type in File.types:
+        File(type, "/")
+
+def test_file_in_set():
+    files = [ File("DIR", "/"), File("DIR", "/"),
+              File("REG", "/"), File("DIR", "/x") ]
+
+    files_set = set(files)
+    # The first two files are identical, so the set should be the same if only
+    # one of them is there and it shold only have 3 items
+    assert_equal(files_set, set(files[1:]))
+    assert_equal(len(files_set), 3)
+
+def test_file_equality():
+    assert_equal(File("DIR", "/"), File("DIR", "/"))
+    assert_not_equal(File("REG", "/"), File("DIR", "/"))
+    assert_not_equal(File("DIR", "/x"), File("DIR", "/y"))
